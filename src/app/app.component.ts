00cdddebf4182d848ff218067570a65dcfda9fe3
jessvilla1975 <jexikvilla@gmail.com>
2024-09-22 14:36:19 -0500

    correcion de css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { RegistroComponent } from "./registro/registro.component";
+import { RegistroComponent } from './components/registro/registro.component';
 
 @Component({
   selector: 'app-root',
