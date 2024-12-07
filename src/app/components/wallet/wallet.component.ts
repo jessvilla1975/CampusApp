dd2f58757a446c15bf179ed2bbaba0fca589a971
Kevin Marin <kealmahe@gmail.com>
2024-12-07 14:40:56 -0500

    Más avances en el wallet

--- src/app/components/wallet/wallet.component.ts
+++ src/app/components/wallet/wallet.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { HeaderPassengerComponent } from '../header-passenger/header-passenger.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wallet',
   standalone: true,
-  imports: [HeaderPassengerComponent, FooterComponent],
+  imports: [HeaderPassengerComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.css'
 })
 export class WalletComponent {
+  locationInput: string = '';
+  locationOutput: string = '';
+  distance: number = 0;
+  duration: number = 0;
+  price: number = 0;
+
+  toggleFormulario() {
+    const formulario = document.getElementById('formulario');
+    if (formulario) {
+      formulario.style.display = formulario.style.display === 'none' ? 'block' : 'none';
+    }
+  }
 
 }
